e6f38a17e78a4f05e5aaa48f81af3846398d7e13
João Targino <joaotarginodev@gmail.com>
2024-11-05 18:13:48 +0000

    feat: created intermediate products enriched table

--- models/staging/erp/products/stg_erp__category.sql
+++ models/staging/erp/products/stg_erp__category.sql
@@ -1,7 +1,7 @@
 with
     rename as (
         select
-            cast(PRODUCTCATEGORYID as int) as pk_subcategory
+            cast(PRODUCTCATEGORYID as int) as pk_category
             , cast(NAME as string) as category_name
         from {{ source('erp_adventureworks', 'productcategory') }}
     )
--- models/staging/erp/products/stg_erp__products.sql
+++ models/staging/erp/products/stg_erp__products.sql
@@ -2,6 +2,7 @@ with
     rename as (
         select
             cast(PRODUCTID as int) as pk_product
+            , cast(PRODUCTSUBCATEGORYID as int) as fk_subcategory
             , cast(NAME as string) as product_name
             , cast(PRODUCTNUMBER as STRING) as product_number
             , cast(SAFETYSTOCKLEVEL as int) as stock
