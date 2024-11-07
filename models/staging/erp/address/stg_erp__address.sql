e72100df633122104820b09b0a2221f07c2d7d06
João Targino <joaotarginodev@gmail.com>
2024-11-07 12:23:46 +0000

    feat: created stg address table

---
+++ models/staging/erp/address/stg_erp__address.sql
@@ -0,0 +1,14 @@
+with
+    rename as (
+        select
+            cast(ADDRESSID as int) as pk_address
+            , case
+                when ADDRESSLINE2 is null then ADDRESSLINE1
+                else ADDRESSLINE1 || ', ' || ADDRESSLINE2
+            end as address
+            , cast(CITY as string) as city
+            , cast(STATEPROVINCEID as int) as fk_state
+            -- Some postal codes use string characters
+            , cast(POSTALCODE as string) as postal_code
+        from {{ source('erp_adventureworks', 'address') }}
+    )
