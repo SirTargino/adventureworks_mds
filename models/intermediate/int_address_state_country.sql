c060bc5e7391f29b0821f56382ca9e10d6b70095
João Targino <joaotarginodev@gmail.com>
2024-11-07 13:20:29 +0000

    feat: created dim_address using data from int_address table

--- models/intermediate/int_address_state_country.sql
+++ models/intermediate/int_address_state_country.sql
@@ -19,11 +19,11 @@ with
             address.pk_address
             , address.address
             , address.city
+            , address.postal_code
             , state.stateprovince_name
             , state.stateprovince_code
             , country.country_name
             , country.pk_countryregion as country_code
-            , address.postal_code
         from address
         left join state on state.pk_stateprovince = address.fk_stateprovince
         left join country on country.pk_countryregion = state.fk_country
