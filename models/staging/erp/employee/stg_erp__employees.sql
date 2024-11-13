6afca01b9e1e160ca4fd534de87464edde3c70fe
João Targino <joaotarginodev@gmail.com>
2024-11-13 13:47:02 +0000

    feat: created dim_employees

--- models/staging/erp/employee/stg_erp__employees.sql
+++ models/staging/erp/employee/stg_erp__employees.sql
@@ -2,7 +2,7 @@ with
     rename as (
         select
             cast(BUSINESSENTITYID as int) as pk_employee
-            , cast(JOBTITLE as string)
+            , cast(JOBTITLE as string) as jobtitle
         from {{ source('erp_adventureworks', 'employee') }}
     )
 
