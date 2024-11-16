68ec130ed12233bce425f46978271a72c7cb934b
João Targino <joaotarginodev@gmail.com>
2024-11-16 22:21:54 +0000

    feat: created sales order header staging table

---
+++ models/staging/erp/sales/stg_erp__salesorderheader.sql
@@ -0,0 +1,19 @@
+with
+    rename as (
+        select
+            cast(SALESORDERID as int) as pk_sales
+            , cast(CUSTOMERID as int) as fk_customer
+            , cast(SALESPERSONID as int) as fk_employee
+            , cast(BILLTOADDRESSID as int) as fk_territory
+            , cast(CREDITCARDID as int) as fk_credit_card
+            , cast(ORDERDATE as date) as order_date
+            , cast(STATUS as int) as order_status
+            , cast(SUBTOTAL as numeric(18,2)) as subtotal
+            , cast(TAXAMT as numeric(18,2)) as taxes
+            , cast(FREIGHT as numeric(18,2)) as FREIGHT
+            , cast(TOTALDUE as numeric(18,2)) as total_due
+        from {{ source('erp_adventureworks', 'salesorderheader') }}
+    )
+
+select *
+from rename
